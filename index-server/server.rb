be877e5a98b865f1113048b447143616bc8ccbd2
Ran <abbshr@outlook.com>
2015-04-19 16:29:40 +0800

    fix bug: add a loop on every thread, hold the connection

--- index-server/server.rb
+++ index-server/server.rb
@@ -15,27 +15,28 @@ include Handle
 Socket.tcp_server_loop 2333 do |socket, client_addrinfo|
   # index server core logic
   Thread.new {
-    # parse the packet
-    packet = JSON.parse socket.recv 1460, :symbolize_names => true
-    packet.merge! :ip => client_addrinfo.ip_address, :port => client_addrinfo.ip_port
-    p packet
-    # fetch the command & handle it
-    case packet[:cmd]
-    when 'registy'
-      res = handle_registy packet
-    when 'push'
-      res = handle_push packet
-    when 'pull'
-      res = handle_pull packet
-    when 'list'
-      res = handle_list packet
-    else
-      res = { :err => TRUE, :msg => "Invaild Command `#{packet[:cmd]}`" }
+    while (recv = parse_packet(socket))
+      packet, err = recv
+      packet.merge! :ip => client_addrinfo.ip_address, :port => client_addrinfo.ip_port
+      puts packet
+      # fetch the command & handle it
+      case packet[:cmd]
+      when 'registy'
+        res = handle_registy packet
+      when 'push'
+        res = handle_push packet
+      when 'pull'
+        res = handle_pull packet
+      when 'list'
+        res = handle_list packet
+      else
+        res = { :err => TRUE, :msg => "Invaild Command `#{packet[:cmd]}`" }
+      end
+      res = JSON.generate res
+      puts res
+      # response to node-client
+      socket.write generate_packet res
+      #socket.close
     end
-    p res
-    res = JSON.generate res
-    # response to node-client
-    socket.write res
-    socket.close
   }
 end
