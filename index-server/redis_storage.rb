5c563915021feec78ccba7807789059d5a0c6aa8
Ran <abbshr@outlook.com>
2015-04-19 16:28:18 +0800

    fix bug: use an unique id to initialize the entry

--- index-server/redis_storage.rb
+++ index-server/redis_storage.rb
@@ -16,33 +16,25 @@ class RedisStorage
 
   def self::find_one origin
     return nil unless self::exist? origin
-    Redis::Hash.new("redis_storage:#{origin}:entry").all
+    Redis::HashKey.new("redis_storage:#{origin}:entry").all
   end
 
   def self::find filename
     return nil unless self::exist? "*/#{filename}"
     redis.keys("redis_storage:*/#{filename}:entry").map do |e|
-      entry = Redis::Hash.new e
-      {
-        :filename => filename,
-        :peer => entry[:peer]
-      }
+      entry = Redis::HashKey.new e
     end
   end
 
   def self::list 
     redis.keys("redis_storage:*").map do |e|
-      enrty = Redis::Hash.new e
-      { :filename => entry[:filename], 
-        :size => entry[:size],
-        :type => entry[:type],
-        :peer => entry[:peer] 
-      } 
+      enrty = Redis::HashKey.new e
     end
   end
 
   def initialize data
     @origin = "#{data[:ip]}-#{data[:port]}/#{data[:filename]}"
+    @entry = Redis::HashKey.new("#{prefix}#{origin}:entry")
     @entry[:filename] = data[:filename]
     @entry[:size] = data[:size]
     @entry[:type] = data[:type]
